ebe0137445385e179dded4edcca7c97214644fa7
kebaHailu <kibromhailu.5513@gmail.com>
2025-04-09 14:54:10 +0300

    setup basic movie card detail

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,47 +1,29 @@
 import { Link } from "react-router-dom";
 
-export const Card = () => {
+export const Card = ({movie}) => {
+  const { id, title, overview, backdrop_path} = movie; 
+  const default_url = "https://www.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600nw-1037719192.jpg";
+  const base_url = backdrop_path ? `https://image.tmdb.org/t/p/w500${backdrop_path}` :default_url;
+
   return (
     <>
       <div className="max-w-sm m-3 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
         <Link to="#">
           <img
             className="rounded-t-lg"
-            src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src={base_url}
             alt=""
           />
         </Link>
         <div className="p-5">
           <Link to="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
+              {title}
             </h5>
           </Link>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+            {overview}
           </p>
-          <Link
-            to="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </Link>
         </div>
       </div>
     </>
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,14 +1,28 @@
+import { useEffect, useState } from "react";
 import { Card } from "../components";
 export const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  const base_url = "";
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=0419218df2164cdf1a6de78c38908643"
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchMovies();
+  }, []);
   return (
     <main className="movie-list-page ">
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">
-          <Card/>
-          <Card/>
-          <Card/>
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
         </div>
       </section>
     </main>
   );
-}
+};
