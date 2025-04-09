26df7af2cf9e4ef92ad444b53694b246536306d7
kebaHailu <kibromhailu.5513@gmail.com>
2025-04-09 13:13:31 +0300

    implement theme with dark mode and save to local storage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import Logo from "../assets/logo.png";
 export const Footer = () => {
   return (
     <footer> 
-      <footer className="bg-white rounded-lg shadow-sm dark:bg-gray-900 m-4">
+      <footer className="bg-gray-50 rounded-lg  border-2 border-gray-300 shadow-sm dark:bg-gray-900 m-4">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <a
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,3 @@
 export {Header} from "./Header";
 export {Footer} from "./Footer";
-
 export {Card} from "./Card";
